d48c54063da34a18ccf56c0f1aa0c9d6c8e0d82b
AHQ Miness <ahqsecret@gmail.com>
2024-10-31 14:09:43 +0530

    fix set_badge_count

--- src/event_loop.rs
+++ src/event_loop.rs
@@ -310,11 +310,8 @@ impl<T> EventLoopWindowTarget<T> {
   /// - **iOS / Android:** Unsupported.
   #[inline]
   pub fn set_badge_count(&self, _badge: Option<i64>, _desktop_filename: Option<String>) {
-    #[cfg(target_os = "linux")]
-    self.p.set_bage_count(_progress, _desktop_filename);
-
-    #[cfg(target_os = "macos")]
-    self.p.set_bage_count(_progress);
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
+    self.p.set_bage_count(_progress, _desktop_filename)
   }
 
   /// Sets the theme for the application.
