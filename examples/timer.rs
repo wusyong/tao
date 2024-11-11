5e90432883dd09421331021b970ca2534bd3a82b
amrbashir <github@amrbashir.me>
2024-11-11 15:46:42 +0200

    deps: remove `instant`
    closes #1006

--- examples/timer.rs
+++ examples/timer.rs
@@ -2,8 +2,8 @@
 // Copyright 2021-2023 Tauri Programme within The Commons Conservancy
 // SPDX-License-Identifier: Apache-2.0
 
-use instant::Instant;
 use std::time::Duration;
+use std::time::Instant;
 
 use tao::{
   event::{Event, StartCause, WindowEvent},
